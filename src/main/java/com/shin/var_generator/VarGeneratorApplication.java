bc89eaff3e125f71eda3e983b88b1ae135cbc62c
shin88 <shin88@ejbt.co.kr>
2019-01-30 09:59:27 +0900

    first commit

---
+++ src/main/java/com/shin/var_generator/VarGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.shin.var_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VarGeneratorApplication.class, args);
+    }
+
+}
+
